6885193d193b7bfe769218188d79f7dcfe845e26
miss-gif <svx327@naver.com>
2024-05-14 18:18:32 +0900

    Feat : Feat : 리액트 마이그레이션 중 6

--- src/components/MainTop.jsx
+++ src/components/MainTop.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "../css/maintop.css";
+import SlideTopMain from "./SlideTopMain";
 
 const MainTop = () => {
   return (
     <>
       <div className="main-top">
-        <div className="main-top-slide br-20">
-          <div className="swiper topslide">
-            <div className="swiper-wrapper"></div>
-            <div className="swiper-pagination"></div>
-          </div>
-        </div>
-
+        <SlideTopMain />
         <div className="main-top-banner br-20">
           <div className="swiper bannerslide">
             <div className="swiper-wrapper"></div>
